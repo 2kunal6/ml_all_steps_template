777b27b99bb502037a750be8da9f130600a22d1b
Kunal Rout <2kunal6@gmail.com>
2024-03-04 17:39:45 +0530

    storing messages in session

--- src/chatbot.py
+++ src/chatbot.py
@@ -8,13 +8,24 @@ st.title("Echo Bot")
 
 chroma_db = ChromaDB()
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
 
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
     if(prompt is not None):
-        st.markdown(chroma_db.get_inference(prompt))
+        response = chroma_db.get_inference(prompt)
+        st.markdown(response)
+        st.session_state.messages.append({"role": "assistant", "content": response})
