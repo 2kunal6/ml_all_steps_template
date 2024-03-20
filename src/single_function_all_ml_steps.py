beaf40dc40b4c3eb2b08ca457d34229f75e8cccf
Kunal Rout <2kunal6@gmail.com>
2024-03-20 16:11:04 +0530

    removing function call and simply using function name

--- src/single_function_all_ml_steps.py
+++ src/single_function_all_ml_steps.py
@@ -4,22 +4,25 @@ import time
 
 
 def load_data():
-    logging.info("Loading Data")
-    time.sleep(5)
-    logging.info("Data Loading Complete")
+    while(True):
+        logging.info("Loading Data")
+        time.sleep(1)
+        logging.info("Data Loading Complete")
 
 
 def train_model():
-    logging.info("Train Model Start")
-    time.sleep(2)
-    logging.info("Train Model Complete")
-    logging.info("Saving New Model Complete")
+    while(True):
+        logging.info("Train Model Start")
+        time.sleep(2)
+        logging.info("Train Model Complete")
+        logging.info("Saving New Model Complete")
 
 
 def simulate_inference_request(query):
-    logging.info(f'Providing inference for {query}')
-    time.sleep(0.2)
-    logging.info(f'Provided inference for {query}')
+    while(True):
+        logging.info(f'Providing inference for {query}')
+        time.sleep(0.2)
+        logging.info(f'Provided inference for {query}')
 
 
 if __name__ == "__main__":
@@ -28,12 +31,17 @@ if __name__ == "__main__":
 
     logging.info("ML Model Deployed")
 
-    load_data_thread = threading.Thread(target=load_data())
+    load_data_thread = threading.Thread(target=load_data, daemon = True)
     load_data_thread.start()
 
-    train_model_thread = threading.Thread(target=train_model)
+    logging.info("Start Training")
+
+    train_model_thread = threading.Thread(target=train_model, daemon = True)
     train_model_thread.start()
 
     logging.info("Simulating Inference Requests")
-    simulate_inference_request_thread = threading.Thread(target=simulate_inference_request('xxx'))
+    simulate_inference_request_thread = threading.Thread(target=simulate_inference_request('xxx'), daemon = True)
     simulate_inference_request_thread.start()
+
+    while True:
+        pass
