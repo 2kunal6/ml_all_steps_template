37e41f5eaeaf9ca567717fd0313ddd87a8417fbe
Kunal Rout <2kunal6@gmail.com>
2024-03-30 17:17:11 +0530

    testing with docker

--- src/docker_compose/load_data/load_data.py
+++ src/docker_compose/load_data/load_data.py
@@ -1,19 +1,23 @@
 from sklearn import neighbors, datasets
 
 import logging
+import os
+import pickle
 import time
 
 
 logging.basicConfig(level = logging.INFO)
 
 # TODO: Do this using cron
-def main():
-    while(True):
-        logging.info("Loading Data")
-        iris = datasets.load_iris()
-        logging.info("Data Loading Complete")
-        time.sleep(24)
+while(True):
+    logging.info("Loading Data")
+    iris = datasets.load_iris()
+    data_file_path = 'data/iris.pkl'
 
+    if os.path.exists(data_file_path):
+        os.remove(data_file_path)
+    with open(data_file_path, 'wb') as bunch:
+        pickle.dump(iris, bunch, protocol=pickle.HIGHEST_PROTOCOL)
 
-if __name__ == '__main__':
-    main()
+    logging.info("Data Loading Complete")
+    time.sleep(24)
