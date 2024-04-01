442e8b89722de7e72040ad09a4e2f2bc85082c70
Kunal Rout <2kunal6@gmail.com>
2024-04-01 17:46:54 +0530

    storing user feedback

--- src/docker_compose/simulate_inference/simulate_inference_fastapi.py
+++ src/docker_compose/simulate_inference/simulate_inference_fastapi.py
@@ -6,6 +6,7 @@ import uvicorn
 
 
 model_file_path = 'data/model.pkl'
+user_feedback_file_path = 'data/user_feedback.txt'
 last_model_update_time = None
 
 app = FastAPI()
@@ -16,13 +17,18 @@ def read_root():
 
 
 @app.get('/predict')
-def read_item(prompt):
+def predict(prompt):
     global last_model_update_time
     if(last_model_update_time == None or int(time.time() - last_model_update_time) >= 24):
         model = pickle.load(open(model_file_path, 'rb'))
         last_model_update_time = time.time()
     prompt = eval(prompt)
     return str(model.predict(prompt))
+
+@app.get('/store_user_feedback')
+def store_user_feedback(user_feedback):
+    with open(user_feedback_file_path, 'a+') as f:
+        f.write(user_feedback)
 
 
 if __name__ == '__main__':
